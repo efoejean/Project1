ea009da7f096c9a45d7414f3659b51f424e69dd4
efoejean <efoejean@gmail.com>
2021-04-29 22:11:35 -0500

    Push the form in the array and write Todos

--- scripts.js
+++ scripts.js
@@ -3,5 +3,9 @@ document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
   const newAppointment = Object.fromEntries(new FormData(event.target));
-  console.log(newAppointment);
+  appointment.push(newAppointment);
+  console.log(appointment);
 });
+// ToDo write a render function to display the array in a table.
+// we will create table and append it to the body.
+// we will use map function.
