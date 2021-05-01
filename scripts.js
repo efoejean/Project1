d23b94d7d2620bd853cb86e16d7111b0fc813b2d
efoejean <efoejean@gmail.com>
2021-04-30 23:55:26 -0500

    Create table, create a render function to map trough through the array and return in as tr in the table

--- scripts.js
+++ scripts.js
@@ -1,11 +1,59 @@
-const appointment = [];
+const table = document.createElement('table');
+
+document.body.appendChild(table);
+
+const appointment = [
+  {
+    Date: '2021-05-05',
+    Name: 'Efoe ',
+    Phone: '6184096527',
+    Price: '$150',
+    Services: 'senegalese',
+    Stylist: 'pamela',
+    Time: '8h',
+  },
+  {
+    Date: '2021-05-06',
+    Name: 'Abel ',
+    Phone: '6184096527',
+    Price: '$200',
+    Services: 'senegalese',
+    Stylist: 'pamela',
+    Time: '9h',
+  },
+  {
+    Date: '2021-06-05',
+    Name: 'Abelito ',
+    Phone: '6184096527',
+    Price: '$200',
+    Services: 'senegalese',
+    Stylist: 'pamela',
+    Time: '9h',
+  },
+];
+
+function renderAppointment(lists) {
+  table.innerHTML = lists
+    .map(
+      ({ Date, Name, Phone, Price, Services, Stylist, Time }) => `<tr>
+  <th>${Date}</th>
+  <th>${Name}</th>
+  <th>${Phone}</th>
+  <th>${Price}</th>
+  <th>${Services}</th>
+  <th>${Stylist}</th>
+  <th>${Time}</th>
+  </tr>`,
+    )
+    .join('');
+}
+renderAppointment(appointment);
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
   const newAppointment = Object.fromEntries(new FormData(event.target));
   appointment.push(newAppointment);
-  console.log(appointment);
+  renderAppointment(appointment);
 });
+
 // ToDo write a render function to display the array in a table.
-// we will create table and append it to the body.
-// we will use map function.
