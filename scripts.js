5f1ea6040a69ab4011edab705eab25a9cd5037e7
efoejean <efoejean@gmail.com>
2021-05-09 11:47:06 -0500

    Copy and a random function from Mdn

--- scripts.js
+++ scripts.js
@@ -111,6 +111,13 @@ function startWithFilter(lists, textInput) {
   return lists.filter(({ Phone, Name }) => (Phone, Name).startsWith(textInput));
 }
 
+// randomn function
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 document.querySelector('form').addEventListener('submit', event => {
   event.preventDefault();
 
